40247d431c654b7dd0d9256a80efb19a82e00185
Jeevanantham13 <55660166+Jeevanantham13@users.noreply.github.com>
2019-12-04 20:04:19 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,4 @@
+a=5
+b=6
+c=a+b
+print("the sum of a & b",c)
---
+++ marks.py
@@ -0,0 +1,15 @@
+name="jeevanantham"
+maths=82
+ds=79
+python=93
+os=88
+total=maths+ds+python+os
+avg=total/4
+print("student name:",name)
+print("the marks scored in 2nd sem")
+print("maths=",maths)
+print("datastructure=",ds)
+print("python=",python)
+print("operating system=",os)
+print("the total marks:",total)
+print("average marks=",avg)
